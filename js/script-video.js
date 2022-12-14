bba20a5765e5bb58c0231db1f0aec474d11ab951
Matheus Ribeiro <omatheusribeiro@outlook.com>
2022-12-13 21:58:27 -0300

    Subindo melhoria

--- js/script-video.js
+++ js/script-video.js
@@ -1,54 +1,94 @@
-const video = document.querySelector('#video');
+const cam = document.getElementById('cam')
+
+const startVideo = () => {
+    navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+        if (Array.isArray(devices)) {
+            devices.forEach(device => {
+                if (device.kind === 'videoinput') {
+                    if (device.label.includes('c922')) {
+                        navigator.getUserMedia(
+                            { video: {
+                                deviceId: device.deviceId
+                            }},
+                            stream => cam.srcObject = stream,
+                            error => console.error(error)
+                        )
+                    }
+                }
+            })
+        }
+    })
+}
+
+const loadLabels = () => {
+    const labels = ['Matheus Ribeiro']
+    return Promise.all(labels.map(async label => {
+        const descriptions = []
+        for (let i = 1; i <= 1; i++) {
+            const img = await faceapi.fetchImage(`/lib/face-api/labels/${label}/${i}.jpg`)
+            const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor()
+            descriptions.push(detections.descriptor)
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    }))
+}
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('models')
-]).then(startVideo);
+    faceapi.nets.tinyFaceDetector.loadFromUri('/lib/face-api/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/lib/face-api/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/lib/face-api/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/lib/face-api/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/lib/face-api/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/lib/face-api/models'),
+]).then(startVideo)
 
-
-async function startVideo(){
-    const constraints = { video: true };
-
-    try{
-        let stream = await navigator.mediaDevices.getUserMedia(constraints);
-
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-            video.play();
-        }
+cam.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(cam)
+    const canvasSize = {
+        width: 500,
+        height: 375
     }
-    catch(error){
-        console.error(error);
-    }
-};
-
-video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-
-    var positionInfo = video.getBoundingClientRect();
-
-    const displaySize = { width: positionInfo.width, height: positionInfo.height };
-
-    faceapi.matchDimensions(canvas, displaySize);
-
+    const labels = await loadLabels()
+    faceapi.matchDimensions(canvas, canvasSize);
+    debugger
+    const div = document.getElementById('div-video')
+    div.appendChild(canvas);
     setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(
-            video,
-            new faceapi.TinyFaceDetectorOptions()
+        const detections = await faceapi
+            .detectAllFaces(
+                cam,
+                new faceapi.TinyFaceDetectorOptions()
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender()
+            .withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, canvasSize)
+        const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+        const results = resizedDetections.map(d =>
+            faceMatcher.findBestMatch(d.descriptor)
         )
-        .withFaceLandmarks()
-        .withFaceExpressions()
-
-        const resizeDetections = faceapi.resizeResults(detections, displaySize);
-
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-
-        faceapi.draw.drawDetections(canvas, resizeDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
-
-    }, 100);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        resizedDetections.forEach(detection => {
+            const { age, gender, genderProbability } = detection
+            new faceapi.draw.DrawTextField([
+                `${parseInt(age, 10)} years`,
+                `${gender} (${parseInt(genderProbability * 100, 10)})`
+            ], detection.detection.box.topRight).draw(canvas)
+        })
+        results.forEach((result, index) => {
+            const box = resizedDetections[index].detection.box
+            const { label, distance } = result
+            new faceapi.draw.DrawTextField([
+                `${label} (${parseInt(distance * 100, 10)})`
+            ], box.bottomRight).draw(canvas)
+        })
+    }, 100)
 })
